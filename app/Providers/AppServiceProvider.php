8926c71ded61e813cdcdc0f1cb93884441415698
madjek <90720831+madjek@users.noreply.github.com>
2021-12-09 13:20:05 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
